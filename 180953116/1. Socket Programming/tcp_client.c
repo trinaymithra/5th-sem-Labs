4e79ff5ca1dadd64f507c98aa5d3d06e5e615026
TRINAY MITHRA <trinaymithra@gmail.com>
2020-12-22 12:02:27 +0530

    Add files via upload

---
+++ 180953116/1. Socket Programming/tcp_client.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include <errno.h>
+
+int main() {
+
+	int sockfd, err_check;
+	struct sockaddr_in server;
+	int server_port;
+	int choice, n, ele, i;
+	int sent, rcvd;
+
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("Socket: ");
+		exit(0);
+	}
+
+	printf("Enter server's port number: \n");
+	scanf("%d", &server_port);
+
+	//complete details in server socket structure
+	server.sin_family = AF_INET;
+	server.sin_port = htons(server_port);
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	bzero(&server.sin_zero, 8);
+
+	//make a connection with server
+	if ((err_check = connect(sockfd, (struct sockaddr *)&server, sizeof(server))) == -1 ) {
+		perror("Connection: ");
+		exit(0);
+	}
+
+	while(1) {
+		printf("Options are: \n1-Search\n2-Sort\n3-Split\n4-Exit\nChoose an option: ");
+		scanf("%d", &choice);
+
+		if (choice == 4) {
+			close(sockfd);
+			break;
+		}
+
+		printf("Enter number of elements in the array: ");
+		scanf("%d", &n);
+
+		int arr[n];
+		for(i=0; i<n; i++) 
+			scanf("%d", &arr[i]);
+
+		sent = send(sockfd, &choice, sizeof(int), 0);
+		if (sent == -1){
+			perror("Error in sending: ");
+			exit(0);
+		}
+
+		sent = send(sockfd, &n, sizeof(int), 0);
+		if (sent == -1){
+			perror("Error in sending: ");
+			exit(0);
+		}
+
+		sent = send(sockfd, arr, sizeof(arr), 0);
+		if (sent == -1){
+			perror("Error in sending: ");
+			exit(0);
+		}		
+		if (choice == 1) {
+			int index;
+
+			printf("Enter the element to be searched in the array: ");
+			scanf("%d", &ele);
+			send(sockfd, &ele, sizeof(int), 0);
+			if (sent == -1){
+			perror("Error in sending: ");
+			exit(0);
+			}
+
+			rcvd = recv(sockfd, &index, sizeof(int), 0);
+			if (rcvd == -1){
+			perror("Error in receiving: ");
+			exit(0);
+			}
+
+			if (index == -1)
+				printf("Element not found in the array\n");
+			else 
+				printf("Element found at %d position\n", index);
+		}
+		else if (choice == 2) {
+			int ret_arr[n];
+			rcvd = recv(sockfd, ret_arr, sizeof(ret_arr), 0);
+			if (rcvd == -1){
+			perror("Error in receiving: ");
+			exit(0);
+			}
+
+			for(i=0; i<n; i++)
+				printf("%d\t", ret_arr[i]);
+			printf("\n");
+		}
+		else if (choice == 3){
+			int odd_arr[n];
+			int even_arr[n];
+			int odd, even;
+
+			rcvd = recv(sockfd, &odd, sizeof(int), 0);
+			rcvd = recv(sockfd, &even, sizeof(int), 0);
+
+			rcvd = recv(sockfd, odd_arr, sizeof(odd_arr), 0);
+			if (rcvd == -1){
+			perror("Error in receiving: ");
+			exit(0);
+			}
+
+			for(i=0; i<odd; i++)
+				printf("%d\t", odd_arr[i]);
+			printf("\n");
+
+			rcvd = recv(sockfd, even_arr, sizeof(even_arr), 0);
+			if (rcvd == -1){
+			perror("Error in receiving: ");
+			exit(0);
+			}
+
+			for(i=0; i<even; i++)
+				printf("%d\t", even_arr[i]);
+			printf("\n");
+
+		}
+	}
+
+
+	return 0;	
+}
+	
+	
---
+++ 180953116/1. Socket Programming/udp_client.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include <errno.h>
+
+int main() {
+	int sockfd, server_port;
+	struct sockaddr_in server;
+	int len;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd == -1){
+		printf("Error in creating socket");
+		exit(0);
+	}
+
+	printf("Enter server port number: \n");
+	scanf("%d", &server_port);
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(server_port);
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	bzero(&server.sin_zero, 8);
+	while (1) {
+
+		char buf[50];
+		printf("Enter a string: (halt to exit)");
+		scanf("%s", buf);
+
+		len = sizeof(server);
+		int sent = sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&server, len);
+
+		char sid[] = "halt";
+		int ch;
+		if ((ch = strcmp(sid, buf))==0) {
+			close(sockfd);
+		}
+		int rcvd = recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&server, &len);
+		printf("%s\n", buf );
+
+		int str_len;
+		int rcvd = recvfrom(sockfd, &str_len, sizeof(int), 0, (struct sockaddr *)&server, &len);
+		printf("Length of the string is %d\n", str_len);
+
+		int arr[5];
+		int rcvd = recvfrom(sockfd, arr, sizeof(arr), 0, (struct sockaddr *)&server, &len);
+		printf("a appeared %d times\ne appeared %d times\ni appeared %d times\no appeared %d times\nu appeared %d times\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
+
+	return 0;
+}
